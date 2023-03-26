e48e4067fce06ae4ab145e941348caee8bf80bf6
Dominastorm <dchawla228@gmail.com>
2023-03-26 15:59:51 +0530

    Format files

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,53 +1,52 @@
 <template>
-    <div class="top-bar">
-      <div class="top-bar-left">
-        <h2>Welcome {{ user }}</h2>
-      </div>
-      <div class="top-bar-right">
-        <router-link to="/search">Search</router-link>
-        <router-link :to="'/profile/' + userId">My Profile</router-link>
-        <a href="#" @click="logout">Logout</a>
-      </div>
+  <div class="top-bar">
+    <div class="top-bar-left">
+      <h2>Welcome {{ user }}</h2>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      user: {
-        type: String,
-        required: true
-      },
-      userId: {
-        type: String,
-        required: true
-      }
+    <div class="top-bar-right">
+      <router-link to="/search">Search</router-link>
+      <router-link :to="'/profile/' + userId">My Profile</router-link>
+      <a href="#" @click="logout">Logout</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: String,
+      required: true
     },
-    methods: {
-      logout() {
-        // Your logout logic goes here
-        console.log('Logging out...')
-      }
+    userId: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    logout() {
+      // Your logout logic goes here
+      console.log('Logging out...')
     }
   }
-  </script>
-  
-  <style>
-  .top-bar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
-    background-color: #333;
-    color: #fff;
-  }
-  .top-bar-right {
-    display: flex;
-    gap: 1rem;
-  }
-  .top-bar-right a {
-    color: #fff;
-    text-decoration: none;
-  }
-  </style>
-  
+}
+</script>
+
+<style>
+.top-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: #fff;
+}
+.top-bar-right {
+  display: flex;
+  gap: 1rem;
+}
+.top-bar-right a {
+  color: #fff;
+  text-decoration: none;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,11 +4,11 @@
     <form @submit.prevent="login">
       <div>
         <label for="email">Email:</label>
-        <input type="email" id="email" v-model="email" required>
+        <input type="email" id="email" v-model="email" required />
       </div>
       <div>
         <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" required>
+        <input type="password" id="password" v-model="password" required />
       </div>
       <div>
         <button type="submit">Login</button>
