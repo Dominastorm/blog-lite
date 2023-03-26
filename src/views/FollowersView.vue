58492707f8fb25b69cff99a204e5d878e4e095fb
Dominastorm <dchawla228@gmail.com>
2023-03-26 16:56:24 +0530

    Format files

--- src/views/FollowersView.vue
+++ src/views/FollowersView.vue
@@ -1,66 +1,65 @@
 <template>
-    <div>
-      <h2>Followers</h2>
-      <ul>
-        <li v-for="follower in followers" :key="follower.id">
-          <router-link :to="'/profile/' + follower.id">{{ follower.username }}</router-link>
-          <button v-if="!follower.followed" @click="follow(follower.id)">Follow</button>
-          <button v-else @click="unfollow(follower.id)">Unfollow</button>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        followers: [
-          {
-            id: 1,
-            username: 'exampleuser1',
-            followed: false
-          },
-          {
-            id: 2,
-            username: 'exampleuser2',
-            followed: false
-          },
-          // Add more followers here
-        ]
+  <div>
+    <h2>Followers</h2>
+    <ul>
+      <li v-for="follower in followers" :key="follower.id">
+        <router-link :to="'/profile/' + follower.id">{{ follower.username }}</router-link>
+        <button v-if="!follower.followed" @click="follow(follower.id)">Follow</button>
+        <button v-else @click="unfollow(follower.id)">Unfollow</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      followers: [
+        {
+          id: 1,
+          username: 'exampleuser1',
+          followed: false
+        },
+        {
+          id: 2,
+          username: 'exampleuser2',
+          followed: false
+        }
+        // Add more followers here
+      ]
+    }
+  },
+  mounted() {
+    // Make an API call to get the IDs of all followers that the logged-in user is currently following
+    const followedIds = [1] // Replace with actual API response
+
+    // Loop through the followers array and update the followed property of each follower accordingly
+    this.followers.forEach((follower) => {
+      follower.followed = followedIds.includes(follower.id)
+    })
+  },
+  methods: {
+    follow(userId) {
+      // Send an API request to follow the user with the given ID
+      console.log('Following user', userId)
+
+      // Update the followed property of the respective follower to true
+      const follower = this.followers.find((follower) => follower.id === userId)
+      if (follower) {
+        follower.followed = true
       }
     },
-    mounted() {
-      // Make an API call to get the IDs of all followers that the logged-in user is currently following
-      const followedIds = [1]; // Replace with actual API response
-  
-      // Loop through the followers array and update the followed property of each follower accordingly
-      this.followers.forEach(follower => {
-        follower.followed = followedIds.includes(follower.id);
-      });
-    },
-    methods: {
-      follow(userId) {
-        // Send an API request to follow the user with the given ID
-        console.log('Following user', userId);
-  
-        // Update the followed property of the respective follower to true
-        const follower = this.followers.find(follower => follower.id === userId);
-        if (follower) {
-          follower.followed = true;
-        }
-      },
-      unfollow(userId) {
-        // Send an API request to unfollow the user with the given ID
-        console.log('Unfollowing user', userId);
-  
-        // Update the followed property of the respective follower to false
-        const follower = this.followers.find(follower => follower.id === userId);
-        if (follower) {
-          follower.followed = false;
-        }
+    unfollow(userId) {
+      // Send an API request to unfollow the user with the given ID
+      console.log('Unfollowing user', userId)
+
+      // Update the followed property of the respective follower to false
+      const follower = this.followers.find((follower) => follower.id === userId)
+      if (follower) {
+        follower.followed = false
       }
     }
   }
-  </script>
-  
+}
+</script>
--- src/views/FollowingView.vue
+++ src/views/FollowingView.vue
@@ -1,38 +1,38 @@
 <template>
     <div>
-      <h2>Following</h2>
-      <ul>
-        <li v-for="user in following" :key="user.id">
-          <router-link :to="'/profile/' + user.id">{{ user.username }}</router-link>
-          <button @click="unfollow(user.id)">Unfollow</button>
-        </li>
-      </ul>
+        <h2>Following</h2>
+        <ul>
+            <li v-for="user in following" :key="user.id">
+                <router-link :to="'/profile/' + user.id">{{ user.username }}</router-link>
+                <button @click="unfollow(user.id)">Unfollow</button>
+            </li>
+        </ul>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
     data() {
-      return {
-        following: [
-          {
-            id: 1,
-            username: 'exampleuser1'
-          },
-          {
-            id: 2,
-            username: 'exampleuser2'
-          },
-          // Add more followed users here
-        ]
-      }
+        return {
+            following: [
+                {
+                    id: 1,
+                    username: 'exampleuser1'
+                },
+                {
+                    id: 2,
+                    username: 'exampleuser2'
+                },
+                // Add more followed users here
+            ]
+        }
     },
     methods: {
-      unfollow(userId) {
-        // Send an API request to unfollow the user with the given ID
-        console.log('Unfollowing user', userId);
-      }
+        unfollow(userId) {
+            // Send an API request to unfollow the user with the given ID
+            console.log('Unfollowing user', userId);
+        }
     }
-  }
-  </script>
+}
+</script>
   
