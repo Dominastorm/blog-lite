b3c53ffb817a0d3e7debeb88279240ed2c6b40f3
Dominastorm <dchawla228@gmail.com>
2023-03-26 15:49:15 +0530

    Add Login + Signup pages

---
+++ src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" required>
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required>
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+    <p>Don't have an account? <router-link to="/signup">Sign up</router-link>.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      // Your login logic goes here
+      console.log('Logging in...')
+    }
+  }
+}
+</script>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <h2>Sign Up</h2>
+      <form @submit.prevent="signup">
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model="name" required>
+        </div>
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="email" required>
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" required>
+        </div>
+        <div></div>
+          <label for="password">Confirm Password:</label>
+          <input type="password" id="password" v-model="password" required>
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+        <p>Already have an account? <router-link to="/login">Log in</router-link>.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: '',
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      signup() {
+        // Your signup logic goes here
+        console.log('Signing up...')
+      }
+    }
+  }
+  </script>
+  
