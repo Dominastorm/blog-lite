632840dfe0d3794dd63e31c3208ab7305a483014
Dominastorm <dchawla228@gmail.com>
2023-03-26 15:56:23 +0530

    Create Top Bar

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,48 +1,53 @@
 <template>
-    <div>
-      <h2>Feed</h2>
-      <ul>
-        <li v-for="post in posts" :key="post.id">
-          <h3>{{ post.title }}</h3>
-          <p>{{ post.content }}</p>
-          <small>By {{ post.author }} on {{ formatDate(post.date) }}</small>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        posts: []
+  <div>
+    <TopBar user="John Doe" :userId="1" />
+    <h2>Feed</h2>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.content }}</p>
+        <small>By {{ post.author }} on {{ formatDate(post.date) }}</small>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TopBar from '@/components/TopBar.vue'
+
+export default {
+  components: {
+    TopBar
+  },
+  data() {
+    return {
+      posts: []
+    }
+  },
+  created() {
+    // Fetch blog posts from your API
+    this.posts = [
+      {
+        id: 1,
+        title: 'First post',
+        content: 'This is the first post.',
+        author: 'John Doe',
+        date: '2022-01-01T00:00:00Z'
+      },
+      {
+        id: 2,
+        title: 'Second post',
+        content: 'This is the second post.',
+        author: 'Jane Doe',
+        date: '2022-01-02T00:00:00Z'
       }
-    },
-    created() {
-      // Fetch blog posts from your API
-      this.posts = [
-        {
-          id: 1,
-          title: 'First post',
-          content: 'This is the first post.',
-          author: 'John Doe',
-          date: '2022-01-01T00:00:00Z'
-        },
-        {
-          id: 2,
-          title: 'Second post',
-          content: 'This is the second post.',
-          author: 'Jane Doe',
-          date: '2022-01-02T00:00:00Z'
-        }
-      ]
-    },
-    methods: {
-      formatDate(dateString) {
-        const date = new Date(dateString)
-        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
-      }
+    ]
+  },
+  methods: {
+    formatDate(dateString) {
+      const date = new Date(dateString)
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
     }
   }
-  </script>
-  
+}
+</script>
