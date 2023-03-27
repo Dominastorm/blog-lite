47f50bcae7b01aceb62ff64c050713804efe24dc
Dominastorm <dchawla228@gmail.com>
2023-03-27 13:56:39 +0530

    Add style to login and signup

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <TopBar authType="signup"/>
     <h2>Login</h2>
     <form @submit.prevent="login">
       <div>
@@ -14,12 +15,17 @@
         <button type="submit">Login</button>
       </div>
     </form>
-    <p>Don't have an account? <router-link to="/signup">Sign up</router-link>.</p>
+    <p>Don't have an account? <router-link to="/signup">Sign up</router-link></p>
   </div>
 </template>
 
 <script>
+import TopBar from '@/components/TopBar.vue';
+
 export default {
+  components : {
+    TopBar
+  },
   data() {
     return {
       email: '',
@@ -34,3 +40,75 @@ export default {
   }
 }
 </script>
+
+<style>
+body {
+  background-color: #2c3e50; /* Dark blue */
+  color: #fff;
+  font-family: sans-serif;
+}
+
+h2 {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 2rem;
+  background-color: #2980b9; /* Light blue */
+  border-radius: 5px;
+}
+
+label {
+  display: block;
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: none;
+  border-radius: 3px;
+}
+
+button[type="submit"] {
+  padding: 0.5rem;
+  background-color: #2c3e50; /* Dark blue */
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+button[type="submit"]:hover {
+  background-color: #34495e; /* Slightly darker shade of dark blue */
+}
+
+p {
+  font-size: 1.2rem;
+  margin-top: 1rem;
+  text-align: center;
+}
+
+a {
+  color: #fff;
+  text-decoration: underline;
+}
+
+a:hover {
+  color: #eee;
+}
+</style>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <TopBar authType='login'/>
     <h2>Sign Up</h2>
     <form @submit.prevent="signup">
       <div>
@@ -14,19 +15,25 @@
         <label for="password">Password:</label>
         <input type="password" id="password" v-model="password" required />
       </div>
-      <div></div>
-      <label for="password">Confirm Password:</label>
-      <input type="password" id="password" v-model="password" required />
+      <div>
+        <label for="password">Confirm Password:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
       <div>
         <button type="submit">Sign Up</button>
       </div>
     </form>
-    <p>Already have an account? <router-link to="/login">Log in</router-link>.</p>
+    <p>Already have an account? <router-link to="/login">Log in</router-link></p>
   </div>
 </template>
 
 <script>
+import TopBar from '../components/TopBar.vue';
+
 export default {
+  components : {
+    TopBar
+  },
   data() {
     return {
       name: '',
@@ -42,3 +49,80 @@ export default {
   }
 }
 </script>
+
+
+<style>
+body {
+  background-color: #2c3e50; /* Dark blue */
+  color: #fff;
+  font-family: sans-serif;
+}
+
+h2 {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 400px;
+  align-items: center;
+  width: 100%;
+  margin: 0 auto;
+  padding: 2rem;
+  background-color: #2980b9; /* Light blue */
+  border-radius: 5px;
+}
+
+label {
+  display: block;
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: none;
+  border-radius: 3px;
+}
+
+button[type="submit"] {
+  padding: 0.5rem;
+  background-color: #2c3e50; /* Dark blue */
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  cursor: pointer;
+  display: block;
+  margin: 0 auto;
+}
+
+button[type="submit"]:hover {
+  background-color: #34495e; /* Slightly darker shade of dark blue */
+}
+
+p {
+  font-size: 1.2rem;
+  margin-top: 1rem;
+  text-align: center;
+}
+
+a {
+  color: #fff;
+  text-decoration: underline;
+}
+
+a:hover {
+  color: #eee;
+}
+</style>
