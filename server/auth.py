e36b4c7ba1a8b5247851cc69bd7dbf9c24352462
Dominastorm <dchawla228@gmail.com>
2023-03-31 01:24:46 +0530

    Set up Login authentication

--- server/auth.py
+++ server/auth.py
@@ -10,6 +10,20 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # Check if user actually exists
+    # Take the user supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
