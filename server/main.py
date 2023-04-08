f5bf4a952319c8c2db623aac562601fbfacbdf29
Dominastorm <dchawla228@gmail.com>
2023-04-08 11:40:44 +0530

    Fix incorrect button value in FollowToggle

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Union
 from flask import jsonify, Blueprint, render_template, request
 from flask_login import login_required, current_user
 
@@ -16,23 +17,7 @@ def index():
 def profile():
     return render_template('profile.html', name=current_user.name)
 
-@main.route('/followers/<int:user_id>', methods=['GET'])
-def get_followers(user_id: int):
-    query = """
-        SELECT * 
-        FROM users 
-        WHERE id IN (
-            SELECT follower_id 
-            FROM user_follows 
-            WHERE following_id = :user_id
-        )
-    """
-    result = db.session.execute(text(query), {'user_id': user_id})
-    follower_list = [{'id': row.id, 'name': row.name} for row in result]
-    return jsonify({'followers': follower_list}), 200
-
-@main.route('/following/<int:user_id>', methods=['GET'])
-def get_following(user_id: int):
+def get_following_list(user_id: int) -> List[Dict[str, Union[str, int, bool]]]:
     query = """
         SELECT * 
         FROM users 
@@ -43,7 +28,32 @@ def get_following(user_id: int):
         )
     """
     result = db.session.execute(text(query), {'user_id': user_id})
-    following_list = [{'id': row.id, 'name': row.name} for row in result]
+    following_list = [{'id': row.id, 'name': row.name, 'followed': True} for row in result]
+    return following_list
+
+def get_follower_list(user_id: int) -> List[Dict[str, Union[str, int, bool]]]:
+    query = """
+        SELECT * 
+        FROM users 
+        WHERE id IN (
+            SELECT follower_id 
+            FROM user_follows 
+            WHERE following_id = :user_id
+        )
+    """
+    result = db.session.execute(text(query), {'user_id': user_id})
+    following_ids = [row['id'] for row in get_following_list(user_id)]
+    follower_list = [{'id': row.id, 'name': row.name, 'followed': True if row.id in following_ids else False} for row in result]
+    return follower_list
+
+@main.route('/followers/<int:user_id>', methods=['GET'])
+def get_followers(user_id: int):
+    follower_list = get_follower_list(user_id)
+    return jsonify({'followers': follower_list}), 200
+
+@main.route('/following/<int:user_id>', methods=['GET'])
+def get_following(user_id: int):
+    following_list = get_following_list(user_id)
     return jsonify({'following': following_list}), 200
 
 @main.route('/follow/', methods=['POST'])
@@ -74,7 +84,7 @@ def unfollow_user():
     if not unfollow:
         return jsonify({'message': 'User already unfollowed.'}), 200
 
-    # Unfollow the user er already fer already f
+    # Unfollow the user
     db.session.delete(unfollow)
     db.session.commit()
     
